79d0937e7c2d17fd42a6ab3c2af9678d26b897cf
duhangyu7702 <770240437@qq.com>
2025-04-06 22:52:16 +0800

    initial commit

--- src/4.py
+++
@@ -1,9 +0,0 @@
-import rospy
-from sensor_msgs.msg import Image
-
-def image_callback(msg):
-    rospy.loginfo("Image width: %d, height: %d", msg.width, msg.height)
-
-rospy.init_node('image_size_listener')
-rospy.Subscriber('/left_camera/image_raw', Image, image_callback)
-rospy.spin()
